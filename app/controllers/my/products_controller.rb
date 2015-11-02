ef1c3a56da82574f37066d816cac47ef83898a6a
Mariko <maripiyok@gmail.com>
2015-11-02 15:01:26 +0900

    ログインユーザー用の共通レイアウト追加。
    ヘッダーからマイページへのリンク追加。

--- app/controllers/my/products_controller.rb
+++ app/controllers/my/products_controller.rb
@@ -1,5 +1,4 @@
-class My::ProductsController < ApplicationController
-  before_action :authenticate_user!
+class My::ProductsController < My::ApplicationController
 
   def index
     @products = current_user.products.order(:updated_at).page params[:page]
