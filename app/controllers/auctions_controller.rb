69ce6c06355d2b42746de2df2baea77e55d2c530
Mariko <maripiyok@gmail.com>
2015-11-06 09:28:15 +0900

    オークション詳細ページ
    出品者のみ情報編集リンク表示。

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,8 +1,18 @@
 class AuctionsController < ApplicationController
+  before_action :set_auction, only: [ :show ]
+
   def index
+    # TODO : 開催中のオークションのみに限定すること
     @auctions = Auction.all.order(:deadline_date).page params[:page]
   end
 
   def show
   end
+
+  private
+
+    def set_auction
+      # 誰でもどのオークション情報でも見れる
+      @auction = Auction.find(params[:id])
+    end
 end
