6020f14accd7d1c6f27dc9e6521f2b88db9ce141
Mariko <maripiyok@gmail.com>
2015-11-06 13:32:27 +0900

    終了したオークション情報も閲覧できるようにした。

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -2,7 +2,13 @@ class AuctionsController < ApplicationController
   before_action :set_auction, only: [ :show ]
 
   def index
-    @auctions = Auction.open.page params[:page]
+    if !params[:state].nil? && params[:state] == 'closed'
+      @index_state = "終了した"
+      @auctions = Auction.not_open.page params[:page]
+    else
+      @index_state = "開催中の"
+      @auctions = Auction.open.page params[:page]
+    end
   end
 
   def show
