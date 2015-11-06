f658f8078a11f617f07ca9a7dfeec2e387a7a383
Mariko <maripiyok@gmail.com>
2015-11-06 10:26:18 +0900

    Add BidsController
    入札新規作成。

---
+++ app/controllers/bids_controller.rb
@@ -0,0 +1,37 @@
+class BidsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_auction
+
+  def new
+    @modal_title = "#{@auction.name} に入札します"
+    @bid = current_user.bids.new(auction: @auction)
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+  
+  def create
+    @bid = current_user.bids.new(bid_params)
+    @bid.auction = @auction
+    if @bid.acceptable_price?
+      if @bid.save
+        redirect_to @auction, notice: '入札しました。'
+      else
+        redirect_to @auction, alert: '入札出来ませんでした。'
+      end
+    else
+      redirect_to @auction, alert: '入札金額が低すぎます。'
+    end
+  end
+
+  private
+
+    def set_auction
+      @auction = Auction.find(params[:auction_id])
+    end
+  
+    def bid_params
+      params.require(:bid).permit(:price)
+    end
+end
--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,4 +3,8 @@ class Bid < ActiveRecord::Base
   belongs_to :auction
 
   validates_presence_of :user_id, :auction_id, :price
+
+  def acceptable_price?
+    self.auction.min_price < self.price ? true : false
+  end
 end
