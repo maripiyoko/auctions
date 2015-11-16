e53b7c5d536bf2913b6c42b45e1a586ec706a2c6
Mariko <maripiyok@gmail.com>
2015-11-16 09:59:51 +0900

    オークション入札時のモーダル、エラーメッセージの表示方法を変更。
    金額が低すぎて入札出来ない場合、リダイレクトせずにモーダル上にメッセージを表示するようにした。
    入札モデル（Bid）のバリデーションエラーを追加。

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -4,9 +4,16 @@ class Bid < ActiveRecord::Base
   has_one :comment
 
   validates_presence_of :user_id, :auction_id, :price
+  validates_numericality_of :price, only_integer: true, greater_than_or_equal_to: 0, less_than: 1_000_000
+
 
   def acceptable_price?
-    self.auction.min_price < self.price ? true : false
+    if self.auction.min_price < self.price
+      true
+    else
+      self.errors.add(:price, '入札金額が低すぎます。')
+      false
+    end
   end
 
   scope :successful, -> { joins(:auction).merge(Auction.closed) }
