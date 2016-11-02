5c3f0e96a130ee7f2f7e864e5171ccabf7bac4af
Mariko <maripiyok@gmail.com>
2016-11-02 10:58:26 +0900

    Add context setting.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_raven_context
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
+  end
+
+  def set_raven_context
+    if user_signed_in?
+      Raven.user_context(id: current_user.id, type: current_user.class.name)
+    end
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
