f12c2bf37c313f63d90bfdb24457469829b688d8
Mariko <maripiyok@gmail.com>
2016-12-01 18:41:52 +0900

    Add shared directory. Add capistrano task for creating secrets.

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :deploy_to, '/var/www/auctions'
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/secrets.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
